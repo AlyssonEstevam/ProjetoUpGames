a2e3f0a3d393394de9a185a917efe4111fd6db1a
Alysson Estevam <alysson.eduardo@smn.com.br>
2020-12-15 21:47:55 -0300

    INFRA - Adição da modelagem inicial do banco de dados para as principais entidades.

---
+++ infra/modelagem001.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE projetoupgames;
+
+USE projetoupgames;
+
+CREATE TABLE Up_TipoUsuario(
+	Cod_SeqTipoUsuario int AUTO_INCREMENT NOT NULL,
+	Nom_TipoUsuario varchar(20),
+	PRIMARY KEY (Cod_SeqTipoUsuario)
+);
+
+CREATE TABLE Up_Usuario(
+	Cod_SeqUsuario int AUTO_INCREMENT NOT NULL,
+	Nom_Nome varchar(100),
+	Nom_Login varchar(30),
+	Nom_Senha varchar(20),
+	Cod_TipoUsuario int,
+	PRIMARY KEY (Cod_SeqUsuario),
+	CONSTRAINT Cod_TipoUsuario_FK FOREIGN KEY (Cod_TipoUsuario) 
+	REFERENCES Up_TipoUsuario(Cod_SeqTipoUsuario)
+);
+
+CREATE TABLE Up_Genero(
+	Cod_SeqGenero int AUTO_INCREMENT NOT NULL,
+	Nom_Nome varchar(20),
+	PRIMARY KEY (Cod_SeqGenero)
+);
+
+CREATE TABLE Up_Jogo(
+	Cod_SeqJogo int AUTO_INCREMENT NOT NULL,
+	Nom_Nome varchar(100),
+	Cod_Genero int,
+	Vlr_Preco float,
+	PRIMARY KEY (Cod_SeqJogo),
+	CONSTRAINT Cod_Genero_FK FOREIGN KEY (Cod_Genero) 
+	REFERENCES Up_Genero(Cod_SeqGenero)
+);
